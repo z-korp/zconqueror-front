52a20a1088082274bfc5ce3a5339a9c69a600b98
Cheelax <thomas.belloc@gmail.com>
2023-09-21 10:56:45 +0200

    use components instead of svg for the army button

--- client/src/components/ui/map/region.tsx
+++ client/src/components/ui/map/region.tsx
@@ -1,6 +1,5 @@
-import React, { createElement } from "react";
-import { useEffect, useRef, useState } from "react";
-import { darken } from "polished";
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 
 interface RegionProps {
   d: string;
@@ -8,6 +7,7 @@ interface RegionProps {
   fillOpacity: number;
   region: string;
   troups?: number;
+  containerRef?: React.MutableRefObject<null>;
 }
 
 const Region: React.FC<RegionProps> = ({
@@ -16,9 +16,9 @@ const Region: React.FC<RegionProps> = ({
   fillOpacity,
   region,
   troups,
+  containerRef,
 }: RegionProps) => {
-  const [circle, setCircle] = useState<JSX.Element | null>(null);
-  const [troupsText, setTroupsText] = useState<JSX.Element | null>(null);
+  const [position, setPosition] = useState<{ x: number; y: number }>();
   const pathRef = useRef<SVGPathElement>(null);
 
   useEffect(() => {
@@ -27,28 +27,24 @@ const Region: React.FC<RegionProps> = ({
       const bbox = path.getBBox();
       const cx = bbox.x + bbox.width / 2;
       const cy = bbox.y + bbox.height / 2;
-      const r = 60;
-      const textX = cx;
-      const textY = cy;
-      const circle = createElement("circle", {
-        cx,
-        cy,
-        r,
-        fill: "red",
-        stroke: darken(0.2, "red"),
-        strokeWidth: 10,
-      });
-      setCircle(circle);
-      const troupsText = createElement("text", {
-        x: textX,
-        y: textY,
-        textAnchor: "middle",
-        dominantBaseline: "middle",
-        fill: "black",
-        fontSize: "24",
-        children: troups,
-      });
-      setTroupsText(troupsText);
+
+      const svgElement = path.ownerSVGElement;
+      if (svgElement) {
+        const point = svgElement.createSVGPoint();
+        point.x = cx;
+        point.y = cy;
+        const ctm = svgElement.getScreenCTM();
+        if (!ctm) return;
+
+        const screenPoint = point.matrixTransform(ctm);
+
+        // Adjust for the SVG's position in the viewport
+        const svgRect = svgElement.getBoundingClientRect();
+        const x = screenPoint.x - svgRect.left;
+        const y = screenPoint.y - svgRect.top;
+
+        setPosition({ x, y });
+      }
     }
   }, [region]);
 
@@ -58,21 +54,33 @@ const Region: React.FC<RegionProps> = ({
   };
 
   return (
-    <g>
+    <>
+      {position &&
+        containerRef &&
+        containerRef.current &&
+        ReactDOM.createPortal(
+          <div
+            className="absolute flex justify-center items-center cursor-pointer bg-red-500 border-2 border-red-700 rounded-full w-8 h-8"
+            style={{
+              top: `calc(${position.y}px - 15px)`,
+              left: `calc(${position.x}px - 15px)`,
+            }}
+            onClick={() => handlePathClick()}
+          >
+            <span className="text-lg text-black">{troups}</span>
+          </div>,
+
+          containerRef.current // render the button directly in the body
+        )}
       <path
+        ref={pathRef}
         d={d}
         fill={fill}
         fillOpacity={fillOpacity}
         onClick={handlePathClick}
-        ref={pathRef}
-      />
-      {circle && troupsText && (
-        <>
-          {circle}
-          {troupsText}
-        </>
-      )}
-    </g>
+      ></path>
+    </>
   );
 };
+
 export default Region;
