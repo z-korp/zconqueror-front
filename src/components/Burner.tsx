f00bd4894ece584659cdfb1519ec3a895e45fa20
0x-alx <alx.charbo@gmail.com>
2023-12-05 21:42:56 +0100

    Rework Burner.tsx UI

--- src/components/Burner.tsx
+++ src/components/Burner.tsx
@@ -1,73 +1,64 @@
 // BurnerActions.tsx
-import React, { useEffect, useState } from 'react';
-import { useDojo } from '@/DojoContext';
+import React, { useEffect, useState } from "react";
+import { useDojo } from "@/DojoContext";
+import { Button } from "./ui/button";
 
 const Burner: React.FC = () => {
-  const {
-    account: { create, list, select, account, isDeploying },
-  } = useDojo();
+	const {
+		account: { create, list, select, account, isDeploying },
+	} = useDojo();
 
-  const [selectedWallet, setSelectedWallet] = useState<string | null>(null);
+	const [selectedWallet, setSelectedWallet] = useState<string | null>(null);
 
-  useEffect(() => {
-    console.log('isDeploying', isDeploying);
-    console.log('account', account);
-    console.log('list', list());
-  }, [isDeploying]);
-  const handleCreate = () => {
-    console.log('create');
-    create();
-    console.log('get list');
-    const array = list();
-    console.log(isDeploying);
-  };
+	useEffect(() => {
+		console.log("isDeploying", isDeploying);
+		console.log("account", account);
+		console.log("list", list());
+	}, [isDeploying]);
+	const handleCreate = () => {
+		console.log("create");
+		create();
+		console.log("get list");
+		const array = list();
+		console.log(isDeploying);
+	};
 
-  const handleSelectWallet = (walletAddress: string) => {
-    setSelectedWallet(walletAddress);
-    select(walletAddress);
-  };
+	const handleSelectWallet = (walletAddress: string) => {
+		setSelectedWallet(walletAddress);
+		select(walletAddress);
+	};
 
-  return (
-    <div className="p-4">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleCreate}>
-        Créer
-      </button>
-      <div>
-        <h3>Wallets:</h3>
-        <ul>
-          {list().map((wallet) => (
-            <li key={wallet.address}>
-              <button
-                className={`${
-                  selectedWallet === wallet.address ? 'bg-green-500' : 'bg-gray-300'
-                } hover:bg-green-700 text-white font-bold py-2 px-4 rounded`}
-                onClick={() => handleSelectWallet(wallet.address)}
-              >
-                {wallet.address}
-              </button>
-            </li>
-          ))}
-        </ul>
-
-        <>
-          <button onClick={create}>{isDeploying ? 'deploying burner' : 'create burner'}</button>
-          <div className="card">
-            select signer:{' '}
-            <select onChange={(e) => select(e.target.value)}>
-              {list().map((account, index) => {
-                return (
-                  <option value={account.address} key={index}>
-                    {account.address}
-                  </option>
-                );
-              })}
-            </select>
-            <p>account: {account.address}</p>
-          </div>
-        </>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex justify-center p-4'>
+			<div className='flex flex-col items-start gap-2'>
+				<h3 className='font-bold text-start'>Wallets:</h3>
+				<ul className='flex flex-col gap-1'>
+					{list().map((wallet) => (
+						<li key={wallet.address}>
+							<button
+								className={`${
+									selectedWallet === wallet.address
+										? "bg-green-500"
+										: "bg-gray-300"
+								} hover:bg-green-700 text-white font-bold py-2 px-4 rounded min-w-[670px]`}
+								onClick={() =>
+									handleSelectWallet(wallet.address)
+								}
+							>
+								{wallet.address}
+							</button>
+						</li>
+					))}
+				</ul>
+				<Button
+					className='self-end'
+					onClick={handleCreate}
+				>
+					{isDeploying ? "Deploying..." : "Create burner"}
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Burner;
