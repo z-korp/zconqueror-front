da97996c1f1c80ff844f31802a9cd2586c6e846a
0x-alx <alx.charbo@gmail.com>
2023-12-07 11:45:27 +0100

    Increase spacing between SidePlayerInfo

--- src/App.tsx
+++ src/App.tsx
@@ -12,44 +12,51 @@ import Burner from './components/Burner';
 import { useDojo } from './DojoContext';
 
 function App() {
-  const { set_ip } = useElementStore((state) => state);
-  const {
-    setup: {
-      components: { Player },
-      systemCalls: { finish },
-    },
-    account: { account },
-  } = useDojo();
-  const { playerIds, players, game } = useComponentStates();
+	const { set_ip } = useElementStore((state) => state);
+	const {
+		setup: {
+			components: { Player },
+			systemCalls: { finish },
+		},
+		account: { account },
+	} = useDojo();
+	const { playerIds, players, game } = useComponentStates();
 
+	const { current_state } = useElementStore((state) => state);
 
-  const { current_state } = useElementStore((state) => state);
+	const isFortifyPanelVisible =
+		current_state === Phase.FORTIFY || current_state === Phase.ATTACK || current_state === Phase.DEPLOY;
 
-  const isFortifyPanelVisible =
-    current_state === Phase.FORTIFY || current_state === Phase.ATTACK || current_state === Phase.DEPLOY;
-
-  return (
-    <TooltipProvider>
-      <NewGame />
-      <Burner />
-      <div className="flex">
-        <div className="w-1/6 mr-4">{isFortifyPanelVisible && <FortifyPanel />}</div>
-        <div className="w-5/6 pr-8">
-          <Map />
-        </div>
-      </div>
-      <div className="absolute top-24 right-0 flex gap-6 flex-col">
-        {playerIds.map((entityId, index) => (
-          <SidePlayerInfo key={index} index={index} entityId={entityId} />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        {playerIds.map((entityId, index) => (
-          <PlayPanel key={index} index={index} entityId={entityId} />
-        ))}
-      </div>
-    </TooltipProvider>
-  );
+	return (
+		<TooltipProvider>
+			<NewGame />
+			<Burner />
+			<div className='flex'>
+				<div className='w-1/6 mr-4'>{isFortifyPanelVisible && <FortifyPanel />}</div>
+				<div className='w-5/6 pr-8'>
+					<Map />
+				</div>
+			</div>
+			<div className='absolute top-24 right-0 flex gap-14 flex-col'>
+				{playerIds.map((entityId, index) => (
+					<SidePlayerInfo
+						key={index}
+						index={index}
+						entityId={entityId}
+					/>
+				))}
+			</div>
+			<div className='flex justify-center'>
+				{playerIds.map((entityId, index) => (
+					<PlayPanel
+						key={index}
+						index={index}
+						entityId={entityId}
+					/>
+				))}
+			</div>
+		</TooltipProvider>
+	);
 }
 
 export default App;
