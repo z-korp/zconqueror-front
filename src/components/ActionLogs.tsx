c78a24723665aa19dfa09c9c82b96ebe52fdae96
Matthias <matthias.monnier@gmail.com>
2024-02-08 21:33:09 +0100

    Add animation for logs panel

--- src/components/ActionLogs.tsx
+++ src/components/ActionLogs.tsx
@@ -3,8 +3,9 @@ import { getIdFromName } from '@/utils/events';
 import { useElementStore } from '@/utils/store';
 import { format } from 'date-fns';
 import React, { useEffect, useRef, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
-//TODO ADD SWAP DE CARDS
+// TODO: add swap de cards
 
 const ActionLogs: React.FC = () => {
   const { logs } = useLogs();
@@ -14,6 +15,8 @@ const ActionLogs: React.FC = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const { setHighlightedRegion } = useElementStore((state) => state);
+  // Dynamically calculate content height for animation
+  const [contentHeight, setContentHeight] = useState(0);
 
   const toggleCollapse = () => setIsCollapsed((prev) => !prev);
   useEffect(() => {
@@ -22,9 +25,10 @@ const ActionLogs: React.FC = () => {
 
   useEffect(() => {
     if (logsRef.current) {
-      logsRef.current.scrollTop = logsRef.current.scrollHeight;
+      console.log(logsRef.current.scrollHeight);
+      setContentHeight(logsRef.current.scrollHeight);
     }
-  }, [logs]);
+  }, [logs, isCollapsed]);
 
   function handleMouseOver(logEntry: string) {
     setHighlightedRegion(getIdFromName(logEntry));
@@ -34,69 +38,81 @@ const ActionLogs: React.FC = () => {
     setHighlightedRegion(null);
   }
 
+  // Animate the container height
+  const springProps = useSpring({
+    to: {
+      height: isCollapsed ? 0 : contentHeight,
+    },
+    from: { height: 0 },
+    config: { tension: 170, friction: 26 },
+  });
+
   return (
     <div className="max-w-xl w-full">
       <div
-        className="flex justify-between items-center px-4 py-2 bg-gray-700 rounded-t cursor-pointer text-white vt323-font"
+        className="flex justify-between items-center p-2 border-b border-gray-600 bg-gray-700 rounded-t cursor-pointer text-white vt323-font"
         onClick={toggleCollapse}
       >
-        <span>Logs</span>
-        <span>{isCollapsed ? '▲' : '▼'}</span>
+        <span>{`Logs (${logs.length})`}</span>
+        <span className="mr-2">{isCollapsed ? '▲' : '▼'}</span>
       </div>
-      {!isCollapsed && (
-        <div ref={logsRef} className="overflow-auto h-48 p-4 bg-gray-700 rounded-b text-start">
-          {logs.map((log, index) => (
-            <div key={index} className="text-sm mb-2 text-left">
-              <span className=" text-white vt323-font">{format(log.timestamp, 'HH:mm:ss')} - </span>
-              {log.type === EventType.Supply && (
-                <>
-                  <span className=" text-white vt323-font">{log.log[0]} </span>
-                  <span
-                    className=" text-white vt323-font underline"
-                    onMouseOver={() => handleMouseOver(log.log[1])}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {log.log[1]}
-                  </span>
-                </>
-              )}
-              {log.type === EventType.Defend && (
-                <>
-                  <span className=" text-white vt323-font">{log.log[0]} </span>
-                  <span
-                    className=" text-white vt323-font underline"
-                    onMouseOver={() => handleMouseOver(log.log[1])}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {log.log[1]}
-                  </span>
-                  <span className=" text-white vt323-font"> {log.log[2]}</span>
-                </>
-              )}
-              {log.type === EventType.Fortify && (
-                <>
-                  <span className=" text-white vt323-font">{log.log[0]} </span>
-                  <span
-                    className=" text-white vt323-font underline"
-                    onMouseOver={() => handleMouseOver(log.log[1])}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {log.log[1]}
-                  </span>
-                  <span className=" text-white vt323-font"> {log.log[2]} </span>
-                  <span
-                    className=" text-white vt323-font underline"
-                    onMouseOver={() => handleMouseOver(log.log[3])}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {log.log[3]}
-                  </span>
-                </>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
+
+      <animated.div style={springProps} ref={logsRef} className="max-h-48 overflow-x-auto scrollbar-custom">
+        {logs.length !== 0 && (
+          <div className="p-2 bg-gray-700 rounded-b text-start">
+            {logs.map((log, index) => (
+              <div key={index} className="text-sm mb-2 text-left">
+                <span className=" text-white vt323-font">{format(log.timestamp, 'HH:mm:ss')} - </span>
+                {log.type === EventType.Supply && (
+                  <>
+                    <span className=" text-white vt323-font">{log.log[0]} </span>
+                    <span
+                      className=" text-white vt323-font underline"
+                      onMouseOver={() => handleMouseOver(log.log[1])}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {log.log[1]}
+                    </span>
+                  </>
+                )}
+                {log.type === EventType.Defend && (
+                  <>
+                    <span className=" text-white vt323-font">{log.log[0]} </span>
+                    <span
+                      className=" text-white vt323-font underline"
+                      onMouseOver={() => handleMouseOver(log.log[1])}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {log.log[1]}
+                    </span>
+                    <span className=" text-white vt323-font"> {log.log[2]}</span>
+                  </>
+                )}
+                {log.type === EventType.Fortify && (
+                  <>
+                    <span className=" text-white vt323-font">{log.log[0]} </span>
+                    <span
+                      className=" text-white vt323-font underline"
+                      onMouseOver={() => handleMouseOver(log.log[1])}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {log.log[1]}
+                    </span>
+                    <span className=" text-white vt323-font"> {log.log[2]} </span>
+                    <span
+                      className=" text-white vt323-font underline"
+                      onMouseOver={() => handleMouseOver(log.log[3])}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {log.log[3]}
+                    </span>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </animated.div>
     </div>
   );
 };
