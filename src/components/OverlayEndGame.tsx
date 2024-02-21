8f3c29a5c2f89cf78aa8fa0663645465cdb96000
ClanCo <corentin.terrie@gmail.com>
2024-02-21 14:10:56 +0100

    styling to match UI

--- src/components/OverlayEndGame.tsx
+++ src/components/OverlayEndGame.tsx
@@ -27,29 +27,41 @@ const OverlayEndGame: React.FC<OverlayEndGameProps> = ({ players }) => {
     <>
       {showOverlay && (
         <div
-          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex flex-col items-center justify-center z-[1000]"
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex flex-col items-center justify-center z-[1000] vt323-font"
           style={{
             background: 'radial-gradient(circle, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.9) 100%)',
           }}
         >
-          <button className="top-0 right-0 text-secondary" onClick={handleCloseOverlay}>
-            X
-          </button>
-          <span className="text-white text-6xl font-bold shadow-outline z-[1001]">{text}</span>
-          <div className="text-white text-lg">
-            {players
-              .filter((player) => player.rank !== 0)
-              .sort((a, b) => a.rank - b.rank)
-              .map((player, index) => (
-                <div className="flex gap-3 items-center" key={index}>
-                  {player.rank === 1 && <Trophy className="w-10 h-10" stroke="gold" />}
-                  {player.rank === 2 && <Medal className="w-10 h-10" stroke="silver" />}
-                  {player.rank === 3 && <Medal className="w-10 h-10" stroke={getColorRGB('bronze')} />}
-                  {player.rank !== 1 && player.rank !== 2 && player.rank !== 3 && <span>{player.rank}</span>}
-                  <img src={avatars[player.index + 1]} alt="player" className="rounded-full object-cover w-14 h-14" />
-                  <h1>{player.name}</h1>
-                </div>
-              ))}
+          <div className="relative flex flex-col items-center w-1/2 rounded-lg bg-stone-700 border-2 border-stone-900 pb-16 pt-16">
+            <div className="absolute top-0 left-0 transform -translate-y-1/2 w-full flex justify-center">
+              <span className="px-6 py-2 text-white text-6xl rounded-lg bg-stone-700 border-2 border-stone-900">
+                {text}
+              </span>
+            </div>
+            <button className="absolute top-0 right-2 text-secondary text-lg" onClick={handleCloseOverlay}>
+              X
+            </button>
+            <div className="flex flex-col items-center justify-center w-full">
+              {players
+                .filter((player) => player.rank !== 0)
+                .sort((a, b) => a.rank - b.rank)
+                .map((player, index) => (
+                  <div className="flex gap-4 items-center justify-center text-white text-lg w-full" key={index}>
+                    {player.rank === 1 && <Trophy className="w-8 h-8" stroke="gold" />}
+                    {player.rank === 2 && <Medal className="w-8 h-8" stroke="silver" />}
+                    {player.rank === 3 && <Medal className="w-8 h-8" stroke={getColorRGB('bronze')} />}
+                    {player.rank !== 1 && player.rank !== 2 && player.rank !== 3 && <span>{player.rank}</span>}
+                    <img
+                      src={avatars[player.index + 1]}
+                      alt="player"
+                      className="rounded-full object-cover w-14 h-14 mx-4"
+                    />
+                    <div className="w-1/2 bg-black p-1 rounded-lg drop-shadow-lg">
+                      <h1>{player.name}</h1>
+                    </div>
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       )}
