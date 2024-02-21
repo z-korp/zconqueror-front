d9d4913324b4f482029f03087555ceb85a4b47ce
ClanCo <corentin.terrie@gmail.com>
2024-02-21 11:58:10 +0100

    leaderboard with image and icon

--- src/components/OverlayEndGame.tsx
+++ src/components/OverlayEndGame.tsx
@@ -1,4 +1,6 @@
+import { Medal, Trophy } from 'lucide-react';
 import React, { useState } from 'react';
+import { avatars } from '../utils/pfps';
 
 interface OverlayEndGameProps {
   players: any;
@@ -7,6 +9,15 @@ interface OverlayEndGameProps {
 const OverlayEndGame: React.FC<OverlayEndGameProps> = ({ players }) => {
   const text = 'Game Over';
   const [showOverlay, setShowOverlay] = useState(true);
+
+  const getColorRGB = (colorName) => {
+    switch (colorName) {
+      case 'bronze':
+        return 'rgb(205, 127, 50)';
+      default:
+        return colorName;
+    }
+  };
 
   const handleCloseOverlay = () => {
     setShowOverlay(false);
@@ -25,10 +36,20 @@ const OverlayEndGame: React.FC<OverlayEndGameProps> = ({ players }) => {
             X
           </button>
           <span className="text-white text-6xl font-bold shadow-outline z-[1001]">{text}</span>
-          <div className="text-white">
-            {players.map((player) => (
-              <h1>{player.name}</h1>
-            ))}
+          <div className="text-white text-lg">
+            {players
+              .filter((player) => player.rank !== 0)
+              .sort((a, b) => a.rank - b.rank)
+              .map((player, index) => (
+                <div className="flex gap-3 items-center" key={index}>
+                  {player.rank === 1 && <Trophy className="w-10 h-10" stroke="gold" />}
+                  {player.rank === 2 && <Medal className="w-10 h-10" stroke="silver" />}
+                  {player.rank === 3 && <Medal className="w-10 h-10" stroke={getColorRGB('bronze')} />}
+                  {player.rank !== 1 && player.rank !== 2 && player.rank !== 3 && <span>{player.rank}</span>}
+                  <img src={avatars[player.index + 1]} alt="player" className="rounded-full object-cover w-14 h-14" />
+                  <h1>{player.name}</h1>
+                </div>
+              ))}
           </div>
         </div>
       )}
