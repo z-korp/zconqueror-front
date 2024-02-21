53cda66bfb44b585e070abead619ca7e7d95a00d
ClanCo <corentin.terrie@gmail.com>
2024-02-21 11:05:21 +0100

    dismiss overlay for debug

--- src/components/OverlayEndGame.tsx
+++ src/components/OverlayEndGame.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface OverlayEndGameProps {
   players: any;
@@ -6,20 +6,33 @@ interface OverlayEndGameProps {
 
 const OverlayEndGame: React.FC<OverlayEndGameProps> = ({ players }) => {
   const text = 'Game Over';
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  const handleCloseOverlay = () => {
+    setShowOverlay(false);
+    console.log('Overlay fermé');
+  };
   return (
-    <div
-      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex flex-col items-center justify-center z-[1000]"
-      style={{
-        background: 'radial-gradient(circle, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.9) 100%)',
-      }}
-    >
-      <span className="text-white text-6xl font-bold shadow-outline z-[1001]">{text}</span>
-      <div className="text-white">
-        {players.map((player) => (
-          <h1>{player.name}</h1>
-        ))}
-      </div>
-    </div>
+    <>
+      {showOverlay && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex flex-col items-center justify-center z-[1000]"
+          style={{
+            background: 'radial-gradient(circle, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.9) 100%)',
+          }}
+        >
+          <button className="top-0 right-0 text-secondary" onClick={handleCloseOverlay}>
+            X
+          </button>
+          <span className="text-white text-6xl font-bold shadow-outline z-[1001]">{text}</span>
+          <div className="text-white">
+            {players.map((player) => (
+              <h1>{player.name}</h1>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
