94575cb547a6ab07f7c087b7d1e029a86fbe89b9
Matthias <matthias.monnier@gmail.com>
2024-06-06 19:41:25 +0200

    https on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import react from '@vitejs/plugin-react';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  server: {
+    https: {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem'),
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
